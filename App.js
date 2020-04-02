f0c6089a649993f4cf55ced485cd026c01f3b127
freeman1306 <zavalkin06@gmail.com>
2020-04-02 08:57:47 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hellooooапаw454dddddewwwwqww1111111asasпаoooow</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
